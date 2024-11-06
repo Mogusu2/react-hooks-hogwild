1d352d00986c41a0e32230e27b8ad07f139f60a7
James Maroko <jamiemaroko41@gmail.com>
2024-11-06 08:50:46 +0300

    Modified App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React from "react";
-import Nav from "./Nav";
-
-import hogs from "../porkers_data";
+// src/App.js
+import React, { useState } from 'react';
+import HogList from './components/HogList';
+import FilterBar from './components/FilterBar';
+import AddHogForm from './components/AddHogForm';
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  const [filterGreased, setFilterGreased] = useState(false);
+  const [sortCriteria, setSortCriteria] = useState('name');
+  const [hiddenHogs, setHiddenHogs] = useState([]);
+  const [hogs, setHogs] = useState(porkersData);
+
+  function addHog(hog) {
+    setHogs([...hogs, hog]);
+  }
+
+  function hideHog(name) {
+    setHiddenHogs([...hiddenHogs, name]);
+  }
+
+  return (
+    <div className="App">
+      <FilterBar setFilterGreased={setFilterGreased} setSortCriteria={setSortCriteria} />
+      <AddHogForm addHog={addHog} />
+      <HogList filterGreased={filterGreased} sortCriteria={sortCriteria} hiddenHogs={hiddenHogs} />
+    </div>
+  );
 }
 
 export default App;
